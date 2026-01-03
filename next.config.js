718e72e1b39bcd23ff8f247753eea1d3ad12969c
FmcStore <guardianmarx@gmail.com>
2026-01-03 17:52:34 +0700

    Add PWA support with next-pwa configuration

---
+++ next.config.js
@@ -0,0 +1,18 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '**' },
+    ],
+    unoptimized: true, // Biar gambar load lancar walau gratisan
+  },
+};
+
+module.exports = withPWA(nextConfig);
